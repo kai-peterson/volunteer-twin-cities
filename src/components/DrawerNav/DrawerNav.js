9ea63dcfaff94adc4cf4c55f8ea8c03690676df8
Kai Peterson <pete9372@umn.edu>
2019-11-18 17:52:58 -0600

    hamburger drawer working with navigation

---
+++ src/components/DrawerNav/DrawerNav.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+// import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+
+import './DrawerNav.css'
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
+function DrawerNav(props) {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(side, false)}
+            onKeyDown={toggleDrawer(side, false)}
+        >
+            <List className="list-container">
+                <ListItem>
+                    <Link to="/home">
+                        <ListItemIcon><img className="list-item" src="images/home-icon.svg" alt="home button icon" /></ListItemIcon>
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link to="/profile">
+                        <ListItemIcon><img className="list-item" src="images/profpic.jpg" alt="profile button icon" /></ListItemIcon>
+                    </Link>
+                </ListItem>
+                <div className="logout-container">
+                <ListItem>
+                    <ListItemIcon className="logout-button" onClick={() => props.dispatch({ type: 'LOGOUT' })}><img className="list-item" src="images/log-out-icon.png" alt="log out icon" /></ListItemIcon>
+                </ListItem>
+                </div>
+            </List>
+        </div>
+    );
+    
+    return (
+        <span>
+            <span className="burger-icon-container" >
+                <Button onClick={toggleDrawer('right', true)}><img className="burger-icon" src="images/burger-menu-icon.png" alt="burger drawer icon" /></Button>
+            </span>
+            <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
+                {sideList('right')}
+            </Drawer>
+        </span>
+    );
+}
+
+export default connect()(DrawerNav);
--- src/components/OrganizationList/OrganizationList.js
+++ src/components/OrganizationList/OrganizationList.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import './OrganizationList.css'
 
 // this could also be written with destructuring parameters as:
 // const OrganizationList = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const OrganizationList = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
+  <>
+    <span>
+      <h1 id="welcome">
+        Welcome, {props.user.username}!
+      </h1>
+    </span>
     <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in link-button" />
-  </div>
+  </>
 );
 
 // Instead of taking everything from state, we just want the user info.
