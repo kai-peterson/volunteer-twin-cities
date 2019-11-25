86d4da6c5b91c4e692b3a352882c4232cfb23077
Kai Peterson <pete9372@umn.edu>
2019-11-25 11:23:44 -0600

    fix calendar/time inputs in create event view

--- src/components/CreateEventView/CreateEventView.js
+++ src/components/CreateEventView/CreateEventView.js
@@ -11,12 +11,10 @@ class CreateEventView extends Component {
             org_id: this.props.match.params.id,
             name: '',
             description: '',
-            start: '',
-            end: '',
+            start: new Date(),
+            end: new Date(),
             req: '',
         },
-        startDate: new Date(),
-        endDate: new Date()
     }
 
     handleChange = (propertyName, event) => {
@@ -29,18 +27,11 @@ class CreateEventView extends Component {
     }
 
     handleDateChange = (prop, date) => {
-        // if (prop === 'start') {
-        //     this.setState({
-        //         startDate: date
-        //     })
-        // }
-        // else if (prop === 'end') {
-        //     this.setState({
-        //         endDate: date
-        //     })
-        // }
         this.setState({
-            [prop]: date
+            event: {
+                ...this.state.event,
+                [prop]: date
+            }
         })
     }
 
@@ -60,17 +51,17 @@ class CreateEventView extends Component {
                     <input className="create-org-input" onChange={(event) => this.handleChange('description', event)} type="text" placeholder="Event description" value={this.state.event.type} />
                     <h4 className="create-org-subheader">Start Date/Time</h4>
                     <DatePicker
-                        selected={this.state.startDate}
-                        // onSelect={this.handleDateChange}
-                        onChange={(date) => this.handleDateChange('startDate', date)}
+                        selected={this.state.event.start}
+                        onSelect={(date) => this.handleDateChange('start', date)}
+                        onChange={(date) => this.handleDateChange('start', date)}
                         showTimeSelect
                         dateFormat="Pp"
                     />
                     <h4 className="create-org-subheader">End Date/Time</h4>
                     <DatePicker
-                        selected={this.state.endDate}
-                        // onSelect={this.handleDateChange}
-                        onChange={(date) => this.handleDateChange('endDate', date)}
+                        selected={this.state.event.end}
+                        onSelect={(date) => this.handleDateChange('end', date)}
+                        onChange={(date) => this.handleDateChange('end', date)}
                         showTimeSelect
                         dateFormat="Pp"
                     />
