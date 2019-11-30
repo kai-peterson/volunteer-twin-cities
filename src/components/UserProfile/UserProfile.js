399c0a3598d301923a3fb9f4ba7975d3628c997d
Kai Peterson <pete9372@umn.edu>
2019-11-30 13:50:32 -0600

    minor syntax fixes

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -13,11 +13,11 @@ import OrganizationListView from '../OrganizationListView/OrganizationListView'
 
 class UserProfile extends Component {
   componentDidMount() {
-    this.props.dispatch({type: 'GET_USER_EVENTS'})
+    this.props.dispatch({ type: 'GET_USER_EVENTS' })
   }
 
   handleClick = () => {
-    
+
   }
 
   render() {
@@ -26,11 +26,17 @@ class UserProfile extends Component {
         <img className="profile-pic" src={this.props.user.profile_pic} alt="" />
         <h1 className="profile-username">{this.props.user.username}</h1>
         <h2>Upcoming events:</h2>
-        <OrganizationListView style={{ height: '38vh' }} listItems={this.props.eventsRootReducer.userEventsReducer} handleClick={this.handleClick}/>
+        <OrganizationListView style={{ height: '38vh' }} listItems={this.props.eventsRootReducer.userEventsReducer} handleClick={this.handleClick} />
         <div className="profile-buttons-container">
           <Button onClick={() => this.props.history.push('/profile/create')} className="profile-button" variant="contained">Create Organization</Button>
           <Button onClick={() => this.props.history.push('/profile/manage')} className="profile-button" variant="contained">Manage Organizations</Button>
         </div>
+        {this.props.user.access_level === 1 &&
+          <>
+            <h2>Admin-only:<br/>Orgs Awaiting Approval</h2>
+            <OrganizationListView style={{ height: '38vh' }} listItems={this.props.eventsRootReducer.userEventsReducer} handleClick={this.handleClick} />
+          </>
+        }
         {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
       </div>
     )
