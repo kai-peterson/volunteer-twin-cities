f480d90ec99221608d158ff605fdd3c99bfb55e9
Kai Peterson <pete9372@umn.edu>
2019-12-10 13:22:52 -0600

    comment out JSON stringify

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -41,7 +41,7 @@ class UserProfile extends Component {
           <>
             <h2>Admin-only:<br />Orgs Awaiting Approval</h2>
             <OrganizationList listItems={this.props.orgsInfoReducer.setPendingOrgsReducer} handleClick={this.handleClick} />
-            <pre>{JSON.stringify(this.props.orgsInfoReducer, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(this.props.orgsInfoReducer, null, 2)}</pre> */}
           </>
         }
         {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
